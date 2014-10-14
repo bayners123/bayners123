b16d62f60bff274154c6d420402b8b7171d12cfa
Charles Baynham <baynhamdesign@gmail.com>
2014-10-14 13:40:32 +0100

    Better setup process

---
+++ setup.sh
@@ -0,0 +1,29 @@
+echo Checking Node.js install...
+
+if node --version &> /dev/null
+then
+  echo Node installed. 
+else
+  echo ---
+  echo
+  echo Node has not been installed: 
+  echo Please Google and install \"Node.js\"
+  echo Or go to http://nodejs.org/download/
+  echo
+  exit 0
+fi
+
+echo Installing grunt packages...
+npm install
+
+echo Setting up server details...
+echo
+chmod +x key-setup.sh
+
+if ./key-setup.sh
+then
+  echo
+  echo Done. 
+  echo
+  echo Run \"npm publish\" to upload any changes to the website. 
+fi
